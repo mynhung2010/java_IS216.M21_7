f4d81363acc7ccb55de9bf47d123f84248d6bd1e
Nguyễn Thành Phát <84914537+thanhphatuit@users.noreply.github.com>
2022-05-12 14:21:15 +0700

    Add files via upload

---
+++ SQL/Belong.sql
@@ -0,0 +1,24 @@
+﻿/*==============================================================*/
+/* Table: Belong                                                */
+/*==============================================================*/
+CREATE TABLE Belong(
+	ItemNo		varchar(5) not null,
+	RoomNo		varchar(5) not null
+);
+--Tạo khóa chính
+ALTER TABLE Belong ADD CONSTRAINT PK_Belong PRIMARY KEY (ItemNo)
+GO
+/*==============================================================*/
+/*      INSERT DỮ LIỆU BẢNG Belong                              */
+/*==============================================================*/
+
+INSERT INTO Belong VALUES ('I001','R100')
+INSERT INTO Belong VALUES ('I002','R101')
+INSERT INTO Belong VALUES ('I003','R102')
+INSERT INTO Belong VALUES ('I004','R103')
+INSERT INTO Belong VALUES ('I005','R104')
+INSERT INTO Belong VALUES ('I006','R105')
+INSERT INTO Belong VALUES ('I007','R106')
+INSERT INTO Belong VALUES ('I008','R107')
+INSERT INTO Belong VALUES ('I009','R108')
+INSERT INTO Belong VALUES ('I010','R109')
