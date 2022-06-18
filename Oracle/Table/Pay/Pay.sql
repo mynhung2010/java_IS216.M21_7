27914e72a4409aa14d1489c0827446d10db65ed0
Nguyễn Thị Thảo Hồng <75102265+NguyenThiThaoHong@users.noreply.github.com>
2022-06-18 21:28:16 +0700

    Upload

--- Oracle/Table/Pay/Pay.sql
+++ Oracle/Table/Pay/Pay.sql
@@ -1,3 +1,4 @@
+
 /*==============================================================*/
 /* Table: Pay                                              */
 /*==============================================================*/
@@ -7,28 +8,44 @@ CREATE TABLE Pay (
 	CusNo			varchar(5) not null,
 	Method			int not null,
 	-- Status:
-		-- 1: dùng COD
-		-- 2: Bằng cách chuyển khoản
+		-- 1: d�ng COD
+		-- 2: B?ng c�ch chuy?n kho?n
 	PaymentDate		date not null,
 	CONSTRAINT CHK_Method CHECK (Method BETWEEN 1 AND 2)	
 );
---- Khóa chính Pay
+--- Kh�a ch�nh Pay
 ALTER TABLE Pay ADD CONSTRAINT PK_Pay PRIMARY KEY(BillNo,CusNo);
 
+ALTER TABLE Pay
+   ADD CONSTRAINT FK_Pay_Pay_Customer FOREIGN KEY (CUSNO)
+      REFERENCES Customer (CUSNO);
 
+
+ALTER TABLE Pay
+   ADD CONSTRAINT FK_Pay_Pay2_BILL FOREIGN KEY (BILLNO)
+      REFERENCES BILL (BILLNO);
+
+	  
 /*==============================================================*/
-/*      INSERT DỮ LIỆU BẢNG Pay		                            */
+/*      INSERT D? LI?U B?NG Pay		                            */
 /*==============================================================*/
 
+INSERT INTO Pay VALUES ('B001','C001',1,TO_DATE('27/06/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B002','C002',2,TO_DATE('15/03/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B003','C003',1,TO_DATE('02/07/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B004','C004',1,TO_DATE('06/05/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B005','C005',2,TO_DATE('08/05/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B006','C006',2,TO_DATE('25/04/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B007','C007',1,TO_DATE('28/06/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B008','C008',2,TO_DATE('23/01/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B009','C009',2,TO_DATE('23/07/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B010','C010',1,TO_DATE('12/06/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B011','C002',2,TO_DATE('27/06/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B012','C003',2,TO_DATE('27/06/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B013','C004',1,TO_DATE('27/06/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B014','C011',1,TO_DATE('22/05/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B015','C012',2,TO_DATE('30/04/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B016','C013',2,TO_DATE('17/04/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B017','C014',1,TO_DATE('27/05/2021','dd/mm/yyyy'));
+INSERT INTO Pay VALUES ('B018','C015',2,TO_DATE('16/07/2021','dd/mm/yyyy'));
 
-
-INSERT INTO Pay VALUES ('B001','C001',1,TO_DATE('22/2/2022','dd/mm/yyyy'));
-INSERT INTO Pay VALUES ('B002','C002',2,TO_DATE('03/3/2022','dd/mm/yyyy'));
-INSERT INTO Pay VALUES ('B003','C003',1,TO_DATE('23/12/2021','dd/mm/yyyy'));
-INSERT INTO Pay VALUES ('B004','C004',1,TO_DATE('06/3/2022','dd/mm/yyyy'));
-INSERT INTO Pay VALUES ('B005','C005',2,TO_DATE('28/2/2022','dd/mm/yyyy'));
-INSERT INTO Pay VALUES ('B006','C006',2,TO_DATE('13/12/2021','dd/mm/yyyy'));
-INSERT INTO Pay VALUES ('B007','C007',1,TO_DATE('20/11/2021','dd/mm/yyyy'));
-INSERT INTO Pay VALUES ('B008','C008',2,TO_DATE('09/10/2021','dd/mm/yyyy'));
-INSERT INTO Pay VALUES ('B009','C009',2,TO_DATE('26/3/2022','dd/mm/yyyy'));
-INSERT INTO Pay VALUES ('B010','C010',1,TO_DATE('17/1/2022','dd/mm/yyyy'));
