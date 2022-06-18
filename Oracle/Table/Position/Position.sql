896202f4145a6cb9118d231ec5a490e4686a94de
Nguyễn Thị Thảo Hồng <75102265+NguyenThiThaoHong@users.noreply.github.com>
2022-06-18 21:28:44 +0700

    Upload

--- Oracle/Table/Position/Position.sql
+++ Oracle/Table/Position/Position.sql
@@ -1,4 +1,4 @@
-/*==============================================================*/
+﻿/*==============================================================*/
 /* Table: Position                                              */
 /*==============================================================*/
 CREATE TABLE Position(
@@ -6,20 +6,29 @@ CREATE TABLE Position(
 	PosName		varchar2(50)	not null,
 	EmpNo		varchar2(5) not null
 );
---Tạo khóa chính
-ALTER TABLE Position ADD CONSTRAINT PK_Position PRIMARY KEY (PosNo)
-GO
+--T?o khóa chính
+ALTER TABLE Position ADD CONSTRAINT PK_Position PRIMARY KEY (PosNo);
+
+ALTER TABLE Position
+   ADD CONSTRAINT FK_Position_IS_EMPLOYEE FOREIGN KEY (EMPNO)
+      REFERENCES EMPLOYEE (EMPNO);
+
 /*==============================================================*/
-/*      INSERT DỮ LIỆU BẢNG Position				*/
+/*      INSERT D? LI?U B?NG Position				            */
 /*==============================================================*/
 
-INSERT INTO Position VALUES('P001','Le Tan','E001');
-INSERT INTO Position VALUES('P002','Phuc vu','E002');
-INSERT INTO Position VALUES('P003','Phuc vu','E003'); 
-INSERT INTO Position VALUES('P004','Phu phong','E004');
-INSERT INTO Position VALUES('P005','Ve sinh','E005');
-INSERT INTO Position VALUES('P006','Quan ly','E006');
-INSERT INTO Position VALUES('P007','Bao ve','E007');  
-INSERT INTO Position VALUES('P008','Le Tan','E008');
-INSERT INTO Position VALUES('P009','Le Tan','E009');
-INSERT INTO Position VALUES('P010','Le Tan','E010');
+INSERT INTO Position VALUES('P001','Lễ Tân','E001');
+INSERT INTO Position VALUES('P002','Phục vụ','E002');
+INSERT INTO Position VALUES('P003','Phục vụ','E003'); 
+INSERT INTO Position VALUES('P004','Phụ phòng','E004');
+INSERT INTO Position VALUES('P005','Vệ sinh','E005');
+INSERT INTO Position VALUES('P006','Quản lý','E016');
+INSERT INTO Position VALUES('P007','Bảo vệ','E007');  
+INSERT INTO Position VALUES('P008','Lễ Tân','E008');
+INSERT INTO Position VALUES('P009','Lễ Tân','E009');
+INSERT INTO Position VALUES('P010','Lễ Tân','E010');
+INSERT INTO Position VALUES('P011','Lễ tân','E011');
+INSERT INTO Position VALUES('P012','Bảo vệ','E012');
+INSERT INTO Position VALUES('P013','Phục vụ','E013');
+INSERT INTO Position VALUES('P014','Vệ sinh','E014');
+INSERT INTO Position VALUES('P015','Phụ phòng','E006');
